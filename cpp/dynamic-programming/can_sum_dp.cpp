220cc3e18eaca0758f7c2ff6c11d07432e5c12e9
hebontes <gukanozadze@gmail.com>
2024-12-05 01:16:13 +0400

    getting bestSum DP

--- cpp/dynamic-programming/can_sum_dp.cpp
+++ cpp/dynamic-programming/can_sum_dp.cpp
@@ -2,7 +2,35 @@
 // Created by gukan on 12/3/2024.
 //
 #include <iostream>
+#include <vector>
 using namespace std;
+
+
+vector<int> howSumTabulation(int targetSum, int a[], int n) {// n -length
+  vector table(targetSum+1, vector<int>(0, {}));
+
+  for (int i = 0; i < targetSum+1; i++) {
+    if (i == 0 || !table[i].empty()) {
+      for (int j = 0; j < n; j++) {
+        int num = a[j];
+        if (i+num < targetSum+1) {
+          vector<int> combination = table[i];
+          combination.push_back(num);
+          if (table[i+num].size() > combination.size() || table[i+num].size() == 0) {
+            table[i+num] = combination;
+          }
+        }
+      }
+    }
+  }
+
+  vector<int> result = table[targetSum];
+  for (int num: result) {
+    cout << num << " ";
+  }
+
+  return {};
+}
 
 
 bool canSumTabulation(int targetSum, int a[], int n){ // n -length
@@ -14,7 +42,8 @@ bool canSumTabulation(int targetSum, int a[], int n){ // n -length
 
   for (int i = 0; i <= targetSum;i++){
     if(tab[i]){
-      for (int num : a){
+      for (int j = 0; j < n; j++){
+        int num = a[j];
         if(tab[i+num] == false){
           tab[i+num] = true;
         }
@@ -27,9 +56,9 @@ bool canSumTabulation(int targetSum, int a[], int n){ // n -length
 
 int main(){
   int n = 3;
-  int targetSum = 7;
-  int a[3] = {5,3,4};
-  cout << canSumTabulation(targetSum, a, n) << endl;
+  int targetSum = 8;
+  int a[n] = {2,3,5};
+  howSumTabulation(targetSum,a,n);
 
   return 0;
 }
