b01f38b25604cbebb463509b4c310841cbe54ede
gukan <gukanozadze@gmail.com>
2025-01-07 21:00:26 +0400

    Graph adjency matrix

--- cpp/aiasoft/graphs/main.cpp
+++ cpp/aiasoft/graphs/main.cpp
@@ -6,8 +6,11 @@
 
 using namespace std;
 
-// Table shows which vertex is connected to which vertex
-// Table is N X N (Vertex X Vertex)
+void addEdge(vector<int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
 void adjency_matrix() {
     int N, M; // N - Vertex - წვერი ||||| M - Edge - წიბო
     cin >> N >> M;
@@ -34,30 +37,24 @@ void adjency_matrix() {
     // 1 3
 }
 
-void addEdge(vector<int> adj[], int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-}
-
 int main() {
-    int n, m;
-    int u, v;
+    int N, M; // N - Vertex - წვერი ||||| M - Edge - წიბო
+    cin >> N >> M;
 
-    cin >> n >> m;
+    int adj[N + 1][N+1] = {0};
 
-    vector<int> adj[n + 1];
 
-    for (int i = 0; i < m; i++) {
-        cin >> u >> v;
-
-        addEdge(adj, u, v);
+    for (int i = 1; i <= M; i++) {
+        int v, u;
+        cin >> v >> u;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
     }
 
-    for (int i = 1; i < n + 1; i++) {
-        cout << i << " - ";
-        for (int j = 0; j < adj[i].size(); j++) {
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
             cout << adj[i][j] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
 }
