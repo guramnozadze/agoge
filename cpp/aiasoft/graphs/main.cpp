a937d07abda0e58727541caa0f7851d9c755b2d7
gukan <gukanozadze@gmail.com>
2025-01-11 05:07:15 +0400

    removed binary serach

--- cpp/aiasoft/graphs/main.cpp
+++ cpp/aiasoft/graphs/main.cpp
@@ -6,25 +6,6 @@
 
 using namespace std;
 
-int binary_search(const vector<int>& arr, int target) {
-    int left = 0;
-    int right = arr.size() - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == target) {
-            return mid; // Target found, return its index
-        } else if (arr[mid] < target) {
-            left = mid + 1; // Search in the right half
-        } else {
-            right = mid - 1; // Search in the left half
-        }
-    }
-
-    return -1; // Target not found
-}
-
 
 // https://www.aiasoft.ge/problem/306
 void mariams_birthday() {
